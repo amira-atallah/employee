200d042c918314a2d1cdc2fa95be0263e8b4f102
amiraatalla <amiraatalla63@gmail.com>
2024-10-10 23:50:43 +0200

    :sparkels:add staff tableMerge branch 'master' of https://github .com/amira-atallah/employee :sparkles:add staff and controller

--- src/main/java/com/amira/employee/Staff.java
+++ src/main/java/com/amira/employee/Staff.java
@@ -2,4 +2,54 @@ package com.amira.employee;
 
 public class Staff {
 
+    private String staffName;
+    private String staffEmail;
+    private String staffPhone;
+    private String staffDepartment;
+    private String staffSalary;
+
+    public Staff(String staffName, String staffEmail, String staffPhone, String staffDepartment, String staffSalary) {
+        this.staffName = staffName;
+        this.staffEmail = staffEmail;
+        this.staffPhone = staffPhone;
+        this.staffDepartment = staffDepartment;
+        this.staffSalary = staffSalary;
+    }
+
+    public Staff() {
+
+    }
+
+
+    public String getStaffName() {
+        return staffName;
+    }
+    public void setStaffName(String staffName) {
+        this.staffName = staffName;
+    }
+    public String getStaffEmail() {
+        return staffEmail;
+    }
+    public void setStaffEmail(String staffEmail) {
+        this.staffEmail = staffEmail;
+    }
+    public String getStaffPhone() {
+        return staffPhone;
+    }
+    public void setStaffPhone(String staffPhone) {
+        this.staffPhone = staffPhone;
+    }
+    public String getStaffDepartment() {
+        return staffDepartment;
+    }
+    public void setStaffDepartment(String staffDepartment) {
+        this.staffDepartment = staffDepartment;
+    }
+    public String getStaffSalary() {
+        return staffSalary;
+    }
+    public void setStaffSalary(String staffSalary) {
+        this.staffSalary = staffSalary;
+    }
+
 }
--- src/main/java/com/amira/employee/StaffController.java
+++ src/main/java/com/amira/employee/StaffController.java
@@ -1,4 +1,37 @@
 package com.amira.employee;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
 public class StaffController {
+
+    List<Staff> allStaff = new ArrayList<>();
+
+
+
+    @GetMapping("/")
+    public String addStaff(Model model) {
+        model.addAttribute("staff", new Staff());
+        return "addstaff";
+    }
+
+    @PostMapping("/submit")
+    public String submit(Staff staff) {
+        allStaff.add(staff);
+         return "redirect:/getallstaff";
+    }
+
+    @GetMapping("/getallstaff")
+    public String listStaffDetails(Model model) {
+        model.addAttribute("allStaff", allStaff);
+        return "getallstaff"; // Return the name of the view as a string
+    }
 }
+
